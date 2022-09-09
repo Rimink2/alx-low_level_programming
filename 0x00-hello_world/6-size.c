f960ce9cbe898786ce9faf477e901e4e3749bcd9
rimink2 <enkaydiva95@gmail.com>
2022-09-08 20:31:05 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,12 @@
  */
 int main(void)
 {
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
 	printf("Size of a char: %1d byte(s)\n", sizeof(c));
 	printf("Size of an int: %1d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %1d byte(s)\n", sizeof(li));
